68598523d5a20e704108b2f7ad47b8d03c870e2c
Kouji <sy135673@gmail.com>
2018-05-07 14:43:06 +1000

    Add files via upload

--- Team.java
+++ Team.java
@@ -1,6 +1,4 @@
-
-
-
+import java.util.*;
 
 public class Team
 {
@@ -35,9 +33,64 @@ public class Team
        return rank;
    }
    
+   public void setRed(int TeamRed)
+   {
+       redcards = TeamRed;
+   }
    
+   public int getRed()
+   {
+       return redcards;
+   }
+   public void setYellow(int TeamYellow)
+   {
+       yellowcards = TeamYellow;
+   }
    
+   public int getYellow()
+   {
+       return yellowcards;
+   }
    
+   public void setWon(int TeamWon)
+   {
+       won = TeamWon;
+   }
+   
+   public int getWon()
+   {
+       return won;
+   }
+   
+   public void setLost(int TeamLost)
+   {
+       lost = TeamLost;
+   }
+   
+   public int getLost()
+   {
+       return lost;
+   }
+   
+   public void setDrawn(int TeamDrawn)
+   {
+       drawn = TeamDrawn;
+   }
+   
+   public int getDrawn()
+   {
+       return drawn;
+   }
+   
+   public void setGoal(int TeamGoal)
+   {
+       goals = TeamGoal;
+   }
+   
+   public int getGoal()
+   {
+       return goals;
+   }
    
   
 }
